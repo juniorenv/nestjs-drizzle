aa41b4ac3345754b0d7fbca0b868f635132ecae5
juniorenv <lightgraphics80@gmail.com>
2025-10-16 22:40:30 +0000

    chore: import DrizzleModule in AppModule
    - Add DrizzleModule to AppModule imports array
    - Explicitly define empty controllers and providers arrays

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,4 +1,9 @@
 import { Module } from "@nestjs/common";
+import { DrizzleModule } from "./drizzle/drizzle.module";
 
-@Module({})
+@Module({
+  imports: [DrizzleModule],
+  controllers: [],
+  providers: [],
+})
 export class AppModule {}
