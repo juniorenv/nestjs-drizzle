53ab6986fbaead41243ff23701563ea4d289de27
juniorenv <lightgraphics80@gmail.com>
2025-10-24 22:54:58 +0000

    chore(app): register UserModule in application root
    - Add UserModule to AppModule imports array
    - Integrate user management functionality into main application
    - Maintain consistent module registration pattern

--- src/app.module.ts
+++ src/app.module.ts
@@ -2,12 +2,14 @@ import { Module } from "@nestjs/common";
 import { DrizzleModule } from "./drizzle/drizzle.module";
 import { PostModule } from "./post/post.module";
 import { ConfigModule } from "@nestjs/config";
+import { UserModule } from "./user/user.module";
 
 @Module({
   imports: [
     ConfigModule.forRoot({ isGlobal: true }),
     DrizzleModule,
     PostModule,
+    UserModule,
   ],
   controllers: [],
   providers: [],
